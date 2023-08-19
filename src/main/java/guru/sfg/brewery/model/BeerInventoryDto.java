efca4927dcb526f67a46af2dd4e870f87fbb7167
david-navruz <selcuk-nevruz@hotmail.com>
2023-08-19 17:30:12 +0200

    updating the inventory service

--- src/main/java/guru/sfg/brewery/model/BeerInventoryDto.java
+++ src/main/java/guru/sfg/brewery/model/BeerInventoryDto.java
@@ -18,6 +18,7 @@ public class BeerInventoryDto {
     private OffsetDateTime createdDate;
     private OffsetDateTime lastModifiedDate;
     private UUID beerId;
+    private String upc;
     private Integer quantityOnHand;
 
 }
